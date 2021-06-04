95c2f1f78da8be321c672bc973668488fd58de45
Fedor Borshev <fedor@borshev.com>
2021-06-04 18:52:42 +0300

    Fancy image loader for slow connection

--- src/components/DiplomaImage.vue
+++ src/components/DiplomaImage.vue
@@ -1,21 +1,41 @@
 <template>
-  <div class="diploma-image">
-    <img class="diploma-image__image" :src="diploma.image" />
+  <div class="diploma-image" :class="{ 'diploma-image--loading': isLoading }">
+    <img class="diploma-image__image" :src="diploma.image" @load="isLoading = false" />
+    <Loader v-if="isLoading" />
   </div>
 </template>
 <script>
+import Loader from "@/components/Loader.vue";
+
 export default {
+  components: {
+    Loader,
+  },
   props: {
     diploma: { type: Object, required: true },
+  },
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  watch: {
+    "$route.params.language": function () {
+      this.isLoading = true;
+    },
   },
 };
 </script>
 
 <style lang="postcss" scoped>
 .diploma-image {
+  position: relative;
   &__image {
     max-height: 85vh;
     max-width: 95vw;
+  }
+  &--loading {
+    opacity: 0.3;
   }
 }
 </style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,26 @@
+<template>
+  <Spinner class="app-loader" size="medium" />
+</template>
+
+<script>
+import Spinner from "vue-simple-spinner";
+export default {
+  components: {
+    Spinner,
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.app-loader {
+  position: absolute;
+  top: 50%;
+  left: 47%;
+  .vue-simple-spinner {
+    opacity: 0.6;
+  }
+  &.non-centered .vue-simple-spinner {
+    margin: 0 !important;
+  }
+}
+</style>
